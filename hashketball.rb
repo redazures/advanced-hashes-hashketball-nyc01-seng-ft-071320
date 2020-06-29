e19f76a1acfbd0ef387fcc090e2bd7db9946b1c4
james wu <jwu_nyc@yahoo.com>
2020-06-29 22:54:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -230,7 +230,8 @@ def big_shoe_rebounds
   shoe_man=0
   x= game_hash[:home][:players]
   x.each do |playa|
-    rebounds = playa[:rebounds], shoe_man=playa[:player_name] if playa[:rebounds]>rebounds
+    rebounds = playa[:rebounds] if playa[:rebounds]>rebounds
+    shoe_man=playa[:player_name] if playa[:rebounds]=rebounds
   end
   y= game_hash[:away][:players]
   x.each do |playa|
